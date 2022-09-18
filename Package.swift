de73a0f96cab123972f0d5823ed018eacbd48ce4
yyokii <yyokii.h@gmail.com>
2022-09-19 00:00:11 +0900

    👷 Add swift lint

--- Package.swift
+++ Package.swift
@@ -17,9 +17,37 @@ let package = Package(
     targets: [
         .target(
             name: "Sanpo",
-            dependencies: []),
+            dependencies: []
+        ),
+        .plugin(
+            name: "SwiftLintPlugin",
+            capability: .buildTool(),
+            dependencies: [
+                .target(name: "SwiftLintBinary"),
+            ]
+        ),
+        .binaryTarget(
+            name: "SwiftLintBinary",
+            url: "https://github.com/realm/SwiftLint/releases/download/0.48.0/SwiftLintBinary-macos.artifactbundle.zip",
+            checksum: "9c255e797260054296f9e4e4cd7e1339a15093d75f7c4227b9568d63edddba50"
+        ),
+        
+
+        // Test
         .testTarget(
             name: "SanpoTests",
             dependencies: ["Sanpo"]),
     ]
 )
+
+// Append common plugins
+package.targets = package.targets.map { target -> Target in
+    if target.type == .regular || target.type == .test {
+        if target.plugins == nil {
+            target.plugins = []
+        }
+        target.plugins?.append(.plugin(name: "SwiftLintPlugin"))
+    }
+
+    return target
+}
