641eb86094a250d13eb343fb5603de58efe95542
yyokii <yyokii.h@gmail.com>
2022-09-26 16:57:22 +0900

    🩹 Fix to reload calendar

--- Sources/HistoricalDataFeature/CalendarView.swift
+++ Sources/HistoricalDataFeature/CalendarView.swift
@@ -4,13 +4,14 @@ import Model
 import Constant
 
 struct CalendarView: UIViewRepresentable {
-    let stepCounts: [Date: StepCount]
+    private let stepCounts: [Date: StepCount]
     @AppStorage(
         UserDefaultsKey.dailyTargetSteps.rawValue,
         store: UserDefaults.app
     )
-    var dailyTargetSteps: Int = 0
-    let selectDateAction: (Date) -> Void
+    private var dailyTargetSteps: Int = 0
+    private let selectDateAction: (Date) -> Void
+    private let calendar = Calendar.current
 
     init(
         stepCounts: [Date: StepCount],
@@ -22,11 +23,10 @@ struct CalendarView: UIViewRepresentable {
 
     func makeUIView(context: Context) -> UICalendarView {
         let view = UICalendarView()
-        view.calendar = Calendar.current
+        view.calendar = self.calendar
 
-        let fromDate = DateComponents(calendar: Calendar.current, year: 2007, month: 1, day: 1).date!
-        let toDate = Date()
-        let calendarViewDateRange = DateInterval(start: fromDate, end: toDate)
+        let calendarStartDate = DateComponents(calendar: self.calendar, year: 2007, month: 1, day: 1).date!
+        let calendarViewDateRange = DateInterval(start: calendarStartDate, end: Date())
         view.availableDateRange = calendarViewDateRange
 
         view.selectionBehavior = UICalendarSelectionSingleDate(delegate: context.coordinator)
@@ -38,11 +38,39 @@ struct CalendarView: UIViewRepresentable {
     func updateUIView(_ uiView: UICalendarView, context: Context) {
         context.coordinator.stepCounts = stepCounts
         context.coordinator.dailyTargetSteps = dailyTargetSteps
-//        uiView.reloadDecorations(forDateComponents: [DateComponents(calendar: Calendar.current, year: 2022, month: 9, day: 8)], animated: true)
+
+        if !stepCounts.isEmpty {
+            /*
+             今現在の月のカレンダーを更新する。
+             表示している月が現在の月でない可能性もあるが、その場合でも現在の月に対してreloadDecorationsを呼ぶことでその月の状態も更新される。
+             「表示している月」に対してreloadDecorationsを呼ぶことがベストであるが、その情報は取得できなさそうなのでこのような対応をしている。
+             */
+            let now = Date()
+            let startOfMonth = calendar.dateInterval(of: .month, for: now)!.start
+
+            let thisMonthDateComponents: [DateComponents] = calendar.array(from: startOfMonth, to: now)
+            uiView.reloadDecorations(forDateComponents: thisMonthDateComponents, animated: false)
+        }
     }
 
     func makeCoordinator() -> Coordinator {
         Self.Coordinator(parent: self)
+    }
+}
+
+public extension Calendar {
+    func array(from startDate: Date, to endDate: Date) -> [DateComponents] {
+        var result: [DateComponents] = []
+
+        let diff = dateComponents([.day], from: startDate, to: endDate).day ?? 0
+        print("diff: \(diff)")
+        for day in 0...diff {
+            let date = date(byAdding: .day, value: day, to: startDate)!
+            let item = dateComponents(in: .current, from: date)
+            result.append(item)
+        }
+
+        return result
     }
 }
 
