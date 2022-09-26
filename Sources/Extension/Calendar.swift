2b84080a0d65f05cc132873251127210c3276c87
yyokii <yyokii.h@gmail.com>
2022-09-26 17:04:54 +0900

    🩹 Add calendar extension

---
+++ Sources/Extension/Calendar.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+public extension Calendar {
+    func array(from startDate: Date, to endDate: Date) -> [DateComponents] {
+        var result: [DateComponents] = []
+
+        let diff = dateComponents([.day], from: startDate, to: endDate).day ?? 0
+        print("diff: \(diff)")
+        for day in 0...diff {
+            let date = date(byAdding: .day, value: day, to: startDate)!
+            let item = dateComponents(in: .current, from: date)
+            result.append(item)
+        }
+
+        return result
+    }
+}
