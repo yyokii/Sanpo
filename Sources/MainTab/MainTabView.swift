24d68b9c25caf4390652c036376bf4673becd88d
yyokii <yyokii.h@gmail.com>
2024-07-27 22:17:25 +0900

    fix: tab

--- Sources/MainTab/MainTabView.swift
+++ Sources/MainTab/MainTabView.swift
@@ -6,53 +6,29 @@ import Model
 
 public struct MainTabView: View {
     @StateObject var weatherData = WeatherData()
-    @State var selectedTab: TabItem = .home
 
     public init() {}
 
     public var body: some View {
-        ZStack(alignment: .bottom) {
-            switch selectedTab {
-            case .home:
-                NavigationStack {
-                    HomeView()
-                }
-                .transition(.move(edge: .leading))
-            case .myPage:
-                NavigationStack {
-                    HistoricalDataView()
-                }
-                .transition(.move(edge: .trailing))
+        TabView {
+            NavigationStack {
+                HomeView()
+            }
+            .tabItem {
+                Image(systemName: "figure.walk")
+                Text("Sanpo")
             }
 
-            SegmentedTabItemView(selectedItem: $selectedTab)
-                .padding(.bottom, 12)
+            NavigationStack {
+                HistoricalDataView()
+            }
+            .tabItem {
+                Image(systemName: "calendar.badge.clock")
+                Text("Data")
+            }
         }
         .environmentObject(weatherData)
-    }
-}
-
-extension MainTabView {
-    func tabItem(of item: TabItem) -> some View {
-        let isSelected = selectedTab == item
-        return Button {
-            withAnimation {
-                selectedTab = item
-            }
-        } label: {
-            HStack(alignment: .center, spacing: 4) {
-                Image(systemName: item.iconName)
-                    .font(.system(size: 16))
-                    .foregroundStyle( isSelected ? .black : .gray)
-                Text(item.title)
-                    .font(.system(size: 18))
-                    .foregroundStyle( isSelected ? .black : .gray)
-            }
-            .bold()
-            .padding(.horizontal, 12)
-            .padding(.vertical, 12)
-            .background(Capsule().fill(isSelected ? .white : .clear))
-        }
+        .tint(.black)
     }
 }
 
